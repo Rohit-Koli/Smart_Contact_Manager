feabbbca9dbe558b85d4a02666bad44096047ffe
Rohit Koli <kolirohit1252@gmail.com>
2024-08-25 10:01:21 +0530

    Changed Welcome MSG

--- SCM-WebApp/src/App.jsx
+++ SCM-WebApp/src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello Rohit!</h1>
+      <h1 className="text-3xl font-bold underline">Hello Tushar!</h1>
+      <h1 className="text-3xl font-bold underline">Hello Meghraj!</h1>
     </>
   );
 }
